39297a51c9b8d36f56c06f32cf4414245244654b
Martin Olmos <martinnolmos@gmail.com>
2023-07-19 09:03:20 -0300

    first commit

---
+++ ejercicio_6.4.js
@@ -0,0 +1,9 @@
+// - Abre una nueva ventana "about:blank" en Google Chrome
+
+// - Abre la consola de desarrollador de Google Chrome (F12)
+
+// - Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+
+const edad = prompt("¿Cuál es tu edad?");
+
+console.log("Edad:", edad);
